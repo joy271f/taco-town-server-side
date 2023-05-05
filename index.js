9eb85c524617d48e498e1e2a0f662a50020fd27a
joychandradas <joychandradas007@gmail.com>
2023-05-05 18:15:27 +0600

    chef detailes dynamic param is ready

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const port = 5000;
 const chef = require("./data/chef.json");
+const chefDetails = require("./data/chefDetails.json");
 
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server is running");
@@ -13,10 +14,22 @@ app.get("/", (req, res) => {
 app.get("/chef", (req, res) => {
   res.send(chef);
 });
+app.get("/chef-details", (req, res) => {
+  res.send(chefDetails);
+});
+
 app.get("/chef/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const selectedChef = chef.find((c) => parseInt(c.id) === id);
   res.send(selectedChef);
+});
+
+app.get("/chef-details/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedChefDetails = chefDetails.filter(
+    (c) => parseInt(c.chef_id) === id
+  );
+  res.send(selectedChefDetails);
 });
 
 app.listen(port, () => {
